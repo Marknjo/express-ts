4b6e8896a91513808aab604f663718d4640dab79
Marknjo <marknjo7@gmail.com>
2022-06-08 11:42:07 +0300

    Refactor homepage handler in the client controller

--- build/controllers/clientController.js
+++ build/controllers/clientController.js
@@ -9,34 +9,8 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.getLoginPage = exports.getHomePage = void 0;
 var html_template_1 = require("../library/views/html-template");
 var decorators_1 = require("../library/decorators");
-var getHomePage = function (req, res) {
-    var loginErrorHtmlTmp = "\n    <p>You are not logged in.</p>\n    <a href=\"/login\" style=\"display: block\">Login Now</a>\n    ";
-    var successLoggedInuserHtmlTmp = "\n    <p>You are currently logged in.</p>\n    <a href=\"/api/v1/users/logout\" style=\"display: block\">Logout</a>\n    ";
-    if (req.session && !req.session.isLoggedIn) {
-        res.send((0, html_template_1.baseHtmlTemplate)(loginErrorHtmlTmp, "Home page"));
-        return;
-    }
-    var sessionExpires = new Date(req.sessionOptions.expires).getTime();
-    var timeDiff = sessionExpires > Date.now();
-    if (!timeDiff) {
-        res.send((0, html_template_1.baseHtmlTemplate)(loginErrorHtmlTmp, "Home page"));
-        return;
-    }
-    res.send((0, html_template_1.baseHtmlTemplate)(successLoggedInuserHtmlTmp, "Home page"));
-};
-exports.getHomePage = getHomePage;
-var getLoginPage = function (req, res) {
-    if (req.session && req.session.isLoginIn) {
-        res.redirect("/");
-        return;
-    }
-    var loginTemplate = "\n     <form \n       action=\"/api/v1/users/login\"\n       method=\"POST\"\n       style=\"display: flex; align-items: center; flex-flow: column nowrap; margin: 20px 40px;\"\n     >\n\n        <div style=\"margin-bottom: 15px; display: flex; align-items: start; flex-flow: column nowrap;\">\n          <label for=\"email\">Email Address</Label>\n          <input type=\"email\" name=\"email\" id=\"email\" required>\n        </div>\n\n\n        <div style=\"margin-bottom: 15px; display: flex; align-items: start; flex-flow: column nowrap;\">\n          <label for=\"password\">Password</Label>\n          <input type=\"password\" name=\"password\" id=\"password\" required>\n        </div>\n\n\n        <div style=\"margin-bottom: 15px; display: block\">\n          <button type=\"submit\"> Login </button>\n        </div>\n     </form>\n  ";
-    res.send((0, html_template_1.baseHtmlTemplate)(loginTemplate, "Login Page"));
-};
-exports.getLoginPage = getLoginPage;
 var ClientController = (function () {
     function ClientController() {
     }
@@ -48,12 +22,33 @@ var ClientController = (function () {
         var loginTemplate = "\n     <form \n       action=\"/api/v1/users/login\"\n       method=\"POST\"\n       style=\"display: flex; align-items: center; flex-flow: column nowrap; margin: 20px 40px;\"\n     >\n\n        <div style=\"margin-bottom: 15px; display: flex; align-items: start; flex-flow: column nowrap;\">\n          <label for=\"email\">Email Address</Label>\n          <input type=\"email\" name=\"email\" id=\"email\" required>\n        </div>\n\n\n        <div style=\"margin-bottom: 15px; display: flex; align-items: start; flex-flow: column nowrap;\">\n          <label for=\"password\">Password</Label>\n          <input type=\"password\" name=\"password\" id=\"password\" required>\n        </div>\n\n\n        <div style=\"margin-bottom: 15px; display: block\">\n          <button type=\"submit\"> Login </button>\n        </div>\n     </form>\n  ";
         res.send((0, html_template_1.baseHtmlTemplate)(loginTemplate, "Login Page"));
     };
+    ClientController.prototype.getHomePage = function (req, res) {
+        var loginErrorHtmlTmp = "\n    <p>You are not logged in.</p>\n    <a href=\"/login\" style=\"display: block\">Login Now</a>\n    ";
+        var successLoggedInuserHtmlTmp = "\n    <p>You are currently logged in.</p>\n    <a href=\"/api/v1/users/logout\" style=\"display: block\">Logout</a>\n    ";
+        if (req.session && !req.session.isLoggedIn) {
+            res.send((0, html_template_1.baseHtmlTemplate)(loginErrorHtmlTmp, "Home page"));
+            return;
+        }
+        var sessionExpires = new Date(req.sessionOptions.expires).getTime();
+        var timeDiff = sessionExpires > Date.now();
+        if (!timeDiff) {
+            res.send((0, html_template_1.baseHtmlTemplate)(loginErrorHtmlTmp, "Home page"));
+            return;
+        }
+        res.send((0, html_template_1.baseHtmlTemplate)(successLoggedInuserHtmlTmp, "Home page"));
+    };
     __decorate([
         (0, decorators_1.Get)("/login"),
         __metadata("design:type", Function),
         __metadata("design:paramtypes", [Object, Object]),
         __metadata("design:returntype", void 0)
     ], ClientController.prototype, "getLoginPage", null);
+    __decorate([
+        (0, decorators_1.Get)("/"),
+        __metadata("design:type", Function),
+        __metadata("design:paramtypes", [Object, Object]),
+        __metadata("design:returntype", void 0)
+    ], ClientController.prototype, "getHomePage", null);
     ClientController = __decorate([
         (0, decorators_1.Controller)(),
         __metadata("design:paramtypes", [])
--- src/controllers/clientController.ts
+++ src/controllers/clientController.ts
@@ -7,96 +7,6 @@ import { Get, Controller } from "../library/decorators";
 
 /// MIDDLEWARE
 
-/// CRUD METHODS
-// @TODO: Home page, login page, dashboard
-
-/// Public handlers
-
-/**
- * Handles Home Page of the app
- * @param req Express request object
- * @param res Express Response object
- * @returns sends response to user based on the condition
- */
-export const getHomePage: RequestHandler = (req, res) => {
-  const loginErrorHtmlTmp = `
-    <p>You are not logged in.</p>
-    <a href="/login" style="display: block">Login Now</a>
-    `;
-
-  const successLoggedInuserHtmlTmp = `
-    <p>You are currently logged in.</p>
-    <a href="/api/v1/users/logout" style="display: block">Logout</a>
-    `;
-
-  /// USER IS NOT LOGGED IN
-
-  /// Show UI based on login status
-  if (req.session && !req.session.isLoggedIn) {
-    res.send(baseHtmlTemplate(loginErrorHtmlTmp, "Home page"));
-
-    return;
-  }
-
-  /// There is session therefore there session options
-  const sessionExpires = new Date(req.sessionOptions.expires!).getTime();
-
-  const timeDiff = sessionExpires > Date.now();
-
-  if (!timeDiff) {
-    res.send(baseHtmlTemplate(loginErrorHtmlTmp, "Home page"));
-
-    return;
-  }
-
-  /// USER IS LOGGED IN
-  res.send(baseHtmlTemplate(successLoggedInuserHtmlTmp, "Home page"));
-};
-
-/**
- * Handles Login Page of the app
- * @param req Express request object
- * @param res Express Response object
- * @returns Renders login html form
- */
-export const getLoginPage: RequestHandler = (req, res) => {
-  // Check if user is logged in -> Send user to home page with message already logged in
-  if (req.session && req.session.isLoginIn) {
-    res.redirect("/");
-    return;
-  }
-
-  // User not logged in -> Send the html template
-  const loginTemplate = `
-     <form 
-       action="/api/v1/users/login"
-       method="POST"
-       style="display: flex; align-items: center; flex-flow: column nowrap; margin: 20px 40px;"
-     >
-
-        <div style="margin-bottom: 15px; display: flex; align-items: start; flex-flow: column nowrap;">
-          <label for="email">Email Address</Label>
-          <input type="email" name="email" id="email" required>
-        </div>
-
-
-        <div style="margin-bottom: 15px; display: flex; align-items: start; flex-flow: column nowrap;">
-          <label for="password">Password</Label>
-          <input type="password" name="password" id="password" required>
-        </div>
-
-
-        <div style="margin-bottom: 15px; display: block">
-          <button type="submit"> Login </button>
-        </div>
-     </form>
-  `;
-
-  res.send(baseHtmlTemplate(loginTemplate, "Login Page"));
-};
-
-/// Admin handlers
-
 //// CONVERT THIS CLASS TO BE A DECORATOR CLASS
 
 @Controller()
@@ -144,6 +54,48 @@ class ClientController {
   `;
 
     res.send(baseHtmlTemplate(loginTemplate, "Login Page"));
+  }
+
+  /**
+   * Handles Home Page of the app
+   * @param req Express request object
+   * @param res Express Response object
+   * @returns sends response to user based on the condition
+   */
+  @Get("/")
+  getHomePage(req: Request, res: Response) {
+    const loginErrorHtmlTmp = `
+    <p>You are not logged in.</p>
+    <a href="/login" style="display: block">Login Now</a>
+    `;
+
+    const successLoggedInuserHtmlTmp = `
+    <p>You are currently logged in.</p>
+    <a href="/api/v1/users/logout" style="display: block">Logout</a>
+    `;
+
+    /// USER IS NOT LOGGED IN
+
+    /// Show UI based on login status
+    if (req.session && !req.session.isLoggedIn) {
+      res.send(baseHtmlTemplate(loginErrorHtmlTmp, "Home page"));
+
+      return;
+    }
+
+    /// There is session therefore there session options
+    const sessionExpires = new Date(req.sessionOptions.expires!).getTime();
+
+    const timeDiff = sessionExpires > Date.now();
+
+    if (!timeDiff) {
+      res.send(baseHtmlTemplate(loginErrorHtmlTmp, "Home page"));
+
+      return;
+    }
+
+    /// USER IS LOGGED IN
+    res.send(baseHtmlTemplate(successLoggedInuserHtmlTmp, "Home page"));
   }
 }
 
